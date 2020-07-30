1d0ae7a7df9471693f8705ee3fe04493e32852d8
Pravesh Chapagain <spravesh1818@gmail.com>
2020-07-30 08:38:58 +0545

    Login page styles finalized and added.Working on styling the chat page.

--- src/components/chatScreenComponent/ChatScreenComponent.js
+++ src/components/chatScreenComponent/ChatScreenComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import io from 'socket.io-client';
 import queryString from 'query-string';
+import "./ChatScreenComponent.css";
 
 let socket;
 const END_POINT = "localhost:5000";
@@ -14,28 +15,28 @@ class ChatScreenComponent extends Component {
             room: "",
             messages: [],
             message: "",
-            users:[]
+            users: []
         }
 
         this.setMessage = this.setMessage.bind(this);
-        this.sendMessage=this.sendMessage.bind(this);
+        this.sendMessage = this.sendMessage.bind(this);
 
     }
 
-    sendMessage(event){
+    sendMessage(event) {
         event.preventDefault();
         console.log(this.state.message);
-        socket.emit('sendMessage',{message:this.state.message},()=>{
+        socket.emit('sendMessage', { message: this.state.message }, () => {
             this.setState({
-                message:''
+                message: ''
             })
         });
     }
-    
+
     setMessage(event) {
         event.preventDefault();
         this.setState({
-            message:event.target.value
+            message: event.target.value
         })
     }
 
@@ -54,57 +55,84 @@ class ChatScreenComponent extends Component {
             room: room
         })
 
-        socket.emit('join', { name, room },(error)=>{
-            if(error){
+        socket.emit('join', { name, room }, (error) => {
+            if (error) {
                 this.props.history.push("/login");
             }
         });
 
-        socket.on('message',(message)=>{
+        socket.on('message', (message) => {
             this.setState({
-                messages:[...this.state.messages,message]
+                messages: [...this.state.messages, message]
             })
         });
 
-        socket.on('usersInRoom',({users})=>{
+        socket.on('usersInRoom', ({ users }) => {
             this.setState({
-                users:users
+                users: users
             })
         })
-        
+
     }
 
-    
+
 
 
     render() {
-    
-        const messages = this.state.messages.map((message) => 
+
+        const messages = this.state.messages.map((message) =>
             <li>{message.user}:{message.text}</li>);
 
-        const onlineUsers = this.state.users.map((user) => 
-            <li>{user.name}</li>);
-       
+        const onlineUsers = this.state.users.map((user) =>
+        <div class="contact">
+        <div class="text">
+            <h4>{user.name}</h4>
+        </div>
+    </div>);
+
         return (<>
-            Welcome {this.state.username}
+            <div className="chat-screen-container">
+                <div className="chat-screen-header">
+                    {/*
+                    an element to show the room
+                    another element to exit the room
+                  */}
+                    <div className="chat-room-name">
+                        <label className="chat-room-name-label">
+                            Room:{this.state.room}
+                        </label>
+                    </div>
+                    <div className="chat-room-exit">
+                        <button className="chat-room-exit-btn">Exit</button>
+                    </div>
+                </div>
+                <div className="chat-screen-body">
+                    <div className="chat-screen-messagebox">
+                        <div className="chat-screen-messages">
+                            <ul>{messages}</ul>
+                        </div>
+                        <div className="chat-screen-message-compose">
+                            <form onSubmit={this.sendMessage}>
+                                <textarea className="chat-screen-input" type="text" value={this.state.message} onChange={this.setMessage}></textarea>
+                                <button type="submit" className="chat-screen-btn">Send</button>
+                            </form>
+                        </div>
 
-            <ul>
-                {messages}
-            </ul>
+                    </div>
+                    <div class="chat-screen-online-user">
+                        <div class="header">
+                            <h2> Online</h2>
+                        </div>
+                        <div class="chat-screen-online-user-list">
+                            {onlineUsers}
+                        </div>
+                    </div>
 
+                </div>
+            </div>
+            <div className="chat-screen-header">
 
-            <p>OnlineUsers</p>
-            <ul>
-                {onlineUsers}
-            </ul>
-
-            <form onSubmit={this.sendMessage}>
-                <input type="text" value={this.state.message} onChange={this.setMessage}></input>
-                <button type="submit">Send</button>
-            </form>
-
-
-
+            </div>
         </>)
     }
 }
--- src/components/loginComponent/LoginComponent.js
+++ src/components/loginComponent/LoginComponent.js
@@ -1,42 +1,52 @@
-import React,{ Component } from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import "./LoginComponent.css";
 
-class LoginComponent extends Component{
+
+class LoginComponent extends Component {
 
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            username:"",
-            room:""
+        this.state = {
+            username: "",
+            room: ""
         }
-        this.submitUsername=this.submitUsername.bind(this);
-        this.submitRoom=this.submitRoom.bind(this);
+        this.submitUsername = this.submitUsername.bind(this);
+        this.submitRoom = this.submitRoom.bind(this);
     }
 
-    submitUsername(event){
+    submitUsername(event) {
         console.log(event.target.value)
         this.setState({
-            username:event.target.value
+            username: event.target.value
         })
     }
 
-    submitRoom(event){
+    submitRoom(event) {
         console.log(event.target.value);
         this.setState({
-            room:event.target.value
+            room: event.target.value
         })
     }
 
 
-    render(){
-        return <div>
-                <input type="text" placeholder="Enter A Username" value={this.state.username} onChange={this.submitUsername}></input>
-                <input type="text" placeholder="Enter A Room To Join" value={this.state.room} onChange={this.submitRoom}></input>
-                <Link onClick={event=>(!this.state.username)?event.preventDefault():null} to={`/chat?name=${this.state.username}&room=${this.state.room}`}>
-                    <button type="submit">Sign In</button>
-                </Link>
-                
+    render() {
+        return <div className="login-container">
+            <div className="login-form">
+                <div className="login-label-div"><label >Login</label></div>
+                <div>
+                    <input type="text" className="login-input" placeholder="Enter A Username" value={this.state.username} onChange={this.submitUsername}></input>
+                </div>
+                <div>
+                    <input type="text" className="login-input" placeholder="Enter A Room To Join" value={this.state.room} onChange={this.submitRoom}></input>
+                </div>
+                <div className="login-row">
+                    <Link onClick={event => (!this.state.username) ? event.preventDefault() : null} to={`/chat?name=${this.state.username}&room=${this.state.room}`}>
+                        <button className="login-btn" type="submit">Sign In</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     }
 }
